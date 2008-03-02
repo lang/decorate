801142adce832ef8038f386f0df1ad676bd0f115
Stefan Lang <langstefan@gmx.at>
2008-03-02 14:20:54 +0100

    Add some API docs, update manifest.

--- lib/decorate.rb
+++ lib/decorate.rb
@@ -2,6 +2,8 @@ module Decorate
 
   VERSION = "0.2.0"
 
+  # A BlockDecorator instance wraps an object (+block+) and delegates
+  # calls to +decorate+ to the wrapped objects +call+ method.
   class BlockDecorator
 
     def initialize(block)
@@ -14,17 +16,42 @@ module Decorate
 
   end
 
+  # Push a decorator on the current threads decorator stack.
+  # A decorator is an object that responds to +decorate+ taking a
+  # class and a method name (symbol) as arguments.
   def self.push_decorator(decorator)
     raise TypeError, "#{decorator} not a decorator" unless decorator
     (Thread.current[:_decorator_stack_] ||= []).push(decorator)
   end
 
+  # Clear the current threads decorator stack. Returns the old stack
+  # as an Enumerable.
   def self.clear_decorators
     stack = (Thread.current[:_decorator_stack_] ||= []).dup
     Thread.current[:_decorator_stack_].clear
     stack
   end
 
+  # Add a decorator that will be applied to the next method
+  # definition. Either pass an object that responds to
+  # decorate(klass, method_name) or a block. The block will be wrapped
+  # in a BlockDecorator.
+  #
+  # Example:
+  #
+  #   def trace_def
+  #     Decorate.decorate { |klass, method_name|
+  #       puts "Method #{method_name.inspect} defined in #{klass.inspect}"
+  #     }
+  #   end
+  #
+  #   class Foo
+  #     trace_def
+  #     def bar
+  #       # ...
+  #     end
+  #   end
+  #   # prints: Method :bar defined in Foo
   def self.decorate(decorator = nil, &block)
     if decorator.nil?
       raise "decorator argument or block required" if block.nil?
@@ -35,6 +62,10 @@ module Decorate
     push_decorator(decorator)
   end
 
+  # Apply the current threads decorator stack to the specified method.
+  #
+  # Decorate hooks into Module#method_added and
+  # Object#singleton_method_added to call this method.
   def self.process_decorators(klass, method_name)
     Decorate.clear_decorators.reverse!.each { |decorator|
       decorator.decorate(klass, method_name)
