802e71d379085b688b3ca47537529f5f940a7ebf
Ivan Necas <necasik@gmail.com>
2011-10-08 11:42:49 +0200

    Give back the documentation for AroundCall

--- lib/decorate/around_decorator.rb
+++ lib/decorate/around_decorator.rb
@@ -4,7 +4,23 @@ module Decorate
   # argument to an around method.
   class AroundCall
 
-    attr_reader :receiver, :message, :wrapped_message, :args,:block
+    # Receiving object.
+    attr_reader :receiver
+
+    # The message that was sent resulting in this around call.
+    attr_reader :message
+
+    # The name of the method that constitutes the "core" behaviour
+    # (behaviour without the around logic).
+    attr_reader :wrapped_message
+
+    # Original argument list.
+    attr_reader :args
+
+    # Original block.
+    attr_reader :block
+
+    # Holds the result of a transfer to the wrapped method.
     attr_accessor :result
 
     def initialize(receiver, message, wrapped_message, args, block)
